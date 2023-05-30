c5b6be926553db681e443466b1dbdf6202bc03fc
joao <pjoaovictor22@hotmail.com>
2023-05-30 09:07:10 -0300

    fix: fixing filter responsivity

--- capputeno/src/components/filter-by-type.tsx
+++ capputeno/src/components/filter-by-type.tsx
@@ -18,8 +18,8 @@ const FilterList = styled.ul`
 const FilterItem = styled.li<FilterItemProps>`
   font-family: inherit;
   font-weight: ${props => props.selected ? '600' : '400'};
-  font-size: 16px;
-  line-height: 22px;
+  font-size: 12px;
+  line-height: 18px;
   text-align: center;
   text-transform: uppercase;
   cursor: pointer;
@@ -27,6 +27,11 @@ const FilterItem = styled.li<FilterItemProps>`
   color: var(--text-dark);
 
   border-bottom: ${props => props.selected ? '4px solid var(--orange-low)' : 'none'};
+
+  @media (min-width: ${props => props.theme.desktopBreakpoint}){
+    font-size: 16px;
+    line-height: 22px;
+  }
 `;
 
 export function FilterByType(){
--- capputeno/src/components/header.tsx
+++ capputeno/src/components/header.tsx
@@ -15,7 +15,7 @@ const TagHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 20px 160px;
+  padding: 12px 24px;
 
   >div{
     display: flex;
@@ -23,18 +23,29 @@ const TagHeader = styled.header`
     justify-content: center;
     gap: 24px;
   }
+
+  @media (min-width: ${props => props.theme.desktopBreakpoint}){
+    padding: 20px 160px;
+  }
 `;
 
- const Logo = styled.a`
+const Logo = styled.a`
   color: var(--logo-color);
-  font-size: 40px;
+  font-size: 20px;
   line-height: 150%;
+
+  @media (min-width: ${props => props.theme.tabletBreakpoint}){
+    font-size: 24px;
+  }
+
+  @media (min-width: ${props => props.theme.desktopBreakpoint}){
+    font-size: 40px;
+  }
 `;
 
 interface HeaderProps{
 
 }
-
 
 export function Header(props:HeaderProps){
   const {setSearch, search} = useFilter()
