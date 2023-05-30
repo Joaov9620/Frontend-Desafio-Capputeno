8c2b27c734254992120c2b7f176757d1fe9b9476
joao <pjoaovictor22@hotmail.com>
2023-05-30 08:53:57 -0300

    refactor: extract providers

--- capputeno/src/app/layout.tsx
+++ capputeno/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Header } from '@/components/header'
 import './globals.css'
 import { Saira } from 'next/font/google'
 import { FilterContextProvider } from '@/contexts/filter-context'
+import { DefaultProviders } from '@/components/default-providers'
 
 const saira = Saira({ 
   weight: ['300', '400', '500', '600'],
@@ -19,12 +20,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={saira.className}>
-        <FilterContextProvider>
+        <DefaultProviders>
           <Header/>
           {children}
-        </FilterContextProvider>
+        </DefaultProviders>
       </body>
     </html>
   )
